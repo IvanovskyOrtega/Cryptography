7e25175017e33fa8702c8df9ccdb8bfa1249635e
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-09-09 13:55:40 -0500

    Falta agregar documentacion
    Que hueva jaja

--- Primer parcial/Algoritmo-DES/archivos.cpp
+++ Primer parcial/Algoritmo-DES/archivos.cpp
@@ -10,14 +10,14 @@ bitset<MAX_SIZE>* archivos::leerArchivo(string nombre){
 	int numeroDeBitsLeidos;
 	bitset<MAX_SIZE>* bitsLeidos = new (nothrow)bitset<MAX_SIZE>();
 	try{
-        ifstream input(nombre,ios::binary);
+        	ifstream input(nombre,ios::binary);
 		input.seekg(0, ios::end);
 		numeroDeBitsLeidos = input.tellg();
 		tamanio = numeroDeBitsLeidos;
 		cout << "Se leyeron " << tamanio << " bits." << endl;
 		input.seekg(0, ios::beg);
-        input.read((char*)bitsLeidos, numeroDeBitsLeidos);
-        input.close();
+        	input.read((char*)bitsLeidos, numeroDeBitsLeidos);
+        	input.close();
 		return bitsLeidos;
 	
 	}
@@ -30,8 +30,8 @@ bitset<MAX_SIZE>* archivos::leerArchivo(string nombre){
 void archivos::escribirArchivo(bitset<MAX_SIZE>* bits, string nombre, int tamanioBits){
 	try{
 		ofstream archivoEncriptado (nombre, ios::binary);
-        archivoEncriptado.write((const char*)bits, tamanioBits);
-        archivoEncriptado.close();
+        	archivoEncriptado.write((const char*)bits, tamanioBits);
+        	archivoEncriptado.close();
 	}
 	catch(const std::invalid_argument& ia){
 		cerr << "Invalid argument: " << ia.what() << '\n';
