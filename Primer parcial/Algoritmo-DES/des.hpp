e2a87420d823626d0e78c3c4f99d63664dccc56e
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-09-08 19:39:59 -0500

    des.hpp

---
+++ Primer parcial/Algoritmo-DES/des.hpp
@@ -0,0 +1,15 @@
+#ifndef _DES_HPP_
+#define _DES_HPP_
+
+namespace des{
+    extern int permutacionInicial[8][8];
+    
+    extern int permutacionInicialInversa[8][8];
+    
+    extern int seleccionEBit[8][6];
+    
+    extern int permutacionP[8][4];
+    void hola();
+}
+
+#endif
