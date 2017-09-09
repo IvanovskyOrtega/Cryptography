b5c034df4def9253a6917283f1f06ab4dbde8758
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-09-08 22:12:01 -0500

    Se corrigieron errores en algunos argumentos de funciones

--- Primer parcial/Algoritmo-DES/des.hpp
+++ Primer parcial/Algoritmo-DES/des.hpp
@@ -2,26 +2,30 @@
 #define _DES_HPP_
 
 namespace des{
-   extern int permutacionInicial[8][8];
-   extern int permutacionInicialInversa[8][8];
-   extern int seleccionEBit[8][6];
-   extern int permutacionP[8][4];
-   std::bitset<64>* aplicarPermutacionInicial(std::bitset<64>* textoPlano);
-   std::bitset<32>* obtenerLn(std::bitset<64>* texto);
-   std::bitset<32>* obtenerRn(std::bitset<64>* texto);
-   std::bitset<64>* concatenarLnRn(std::bitset<32>* Ln, std::bitset<32>* Rn);
-   std::bitset<48>* aplicarExpansion(std::bitset<32>* texto);
-   std::bitset<48>* aplicarXORConLlave(std::bitset<48>* texto, std::bitset<48>* llave);
-   std::bitset<32>* aplicarCajas(std::bitset<48>* texto);
-   std::bitset<32>* aplicarPermutacionP(std::bitset<32>* texto);
-   std::bitset<32>* aplicarXORLnRn(std::bitset<32>* Ln, std::bitset<32>*Rn);
-   std::bitset<64>* leerLlaveMaestra();
-   std::bitset<56>* aplicarPermutacionPC1(std::bitset<64>* llaveMaestra);
-   std::bitset<28>* obtenerCn(std::bitset<56>* llave);
-   std::bitset<28>* obtenerDn(std::bitset<56>* llave);
-   std::bitset<28>* aplicarCorrimientoCircular(std::bitset<56>* texto);
-   std::bitset<56>* concatenarCnDn(std::bitset<28>* Ln, std::bitset<28>* Rn);
-   std::bitset<48>* aplicarPermutacionPC2(std::bitset<56>* CnDn);
+   
+    extern int permutacionInicial[64];
+    extern int permutacionInicialInversa[64];
+    extern int seleccionEBit[48];
+    extern int permutacionP[32];
+    extern int cajas[8][4][16];
+    extern int permutacionPC1[56];
+    extern int permutacionPC2[48];
+    std::bitset<64>* aplicarPermutacionInicial(std::bitset<64>* textoPlano);
+    std::bitset<32>* obtenerLn(std::bitset<64>* texto);
+    std::bitset<32>* obtenerRn(std::bitset<64>* texto);
+    std::bitset<64>* concatenarLnRn(std::bitset<32>* Ln, std::bitset<32>* Rn);
+    std::bitset<48>* aplicarExpansion(std::bitset<32>* texto);
+    std::bitset<48>* aplicarXORConLlave(std::bitset<48>* texto, std::bitset<48>* llave);
+    std::bitset<32>* aplicarCajas(std::bitset<48>* texto);
+    std::bitset<32>* aplicarPermutacionP(std::bitset<48>* texto);
+    std::bitset<32>* aplicarXORLmRn(std::bitset<32>* Ln, std::bitset<32>*Rn);
+    std::bitset<64>* leerLlaveMaestra();
+    std::bitset<56>* aplicarPermutacionPC1(std::bitset<64>* llaveMaestra);
+    std::bitset<28>* obtenerCn(std::bitset<56>* llave);
+    std::bitset<28>* obtenerDn(std::bitset<56>* llave);
+    std::bitset<28>* aplicarCorrimientoCircular(std::bitset<28>* texto, int numeroDeCorrimientos);
+    std::bitset<56>* concatenarCnDn(std::bitset<28>* Cn, std::bitset<28>* Dn);
+    std::bitset<48>* aplicarPermutacionPC2(std::bitset<56>* CnDn);
 }
 
 #endif
