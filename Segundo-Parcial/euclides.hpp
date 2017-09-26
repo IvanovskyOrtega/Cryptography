1da36fa2e4b03de752f437a7b69909de9ee8d04e
ivanovsky <ivanovsky@ivanovsky-ubuntu>
2017-09-26 07:55:42 -0500

    euclides.hpp

---
+++ Segundo-Parcial/euclides.hpp
@@ -0,0 +1,14 @@
+#ifndef _EUCLIDES_HPP_
+#define _EUCLIDES_HPP_
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+namespace euclides{
+    extern std::vector<int> Qn;
+    int algoritmoDeEuclides(int r0, int r1 );
+    int encontrarInverso(int r0);
+}
+
+#endif
