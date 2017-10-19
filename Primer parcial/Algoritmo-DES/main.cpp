06ae873abde39f6e36cd3fab3cc6eeea4d9e2a6c
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-10-18 23:03:59 -0500

    Ya encripta archivos, falta decriptación

--- Primer parcial/Algoritmo-DES/main.cpp
+++ Primer parcial/Algoritmo-DES/main.cpp
@@ -6,20 +6,27 @@ using namespace archivos;
 using namespace des;
 
 int main (int argc, char* argv[]) {
-	bitset<64>* entrada = new (nothrow)bitset<64>();
-	bitset<64> entrada2(string("0000000100100011010001010110011110001001101010111100110111101111"));
-	cout << "El texto plano a encriptar es: ";
-	for(int i = 0 ; i < 64 ; i++){
-		(*entrada)[i] = entrada2[i];
+	if(argc >= 3){
+		cout << "Archivo a encriptar: " << argv[1] << endl;
+		vector<bitset<64>*> bytesLeidos  = leerArchivo(argv[1]);
+		if(bytesLeidos.size() > 0){
+			vector<bitset<64>*> encriptado;
+			bitset<64>* aux;
+			printf("Tamanio del archivo: %lu\n",tamanio);
+			for(int i = 0 ; i < (int)bytesLeidos.size()-1 ; i++){
+				aux = iniciarEncriptacionDES(bytesLeidos[i]);
+				encriptado.reserve(1);
+				encriptado.push_back(new bitset<64>(*aux));
+				aux->reset();
+				cout << hex << (*aux).to_ulong() << endl;
+			}
+			encriptado.reserve(1);
+			encriptado.push_back(bytesLeidos[bytesLeidos.size()-1]);
+			escribirArchivo(encriptado,argv[2],tamanio);
+		}
 	}
-	cout << hex << (*entrada).to_ulong() << endl;
-	iniciarEncriptacionDES(entrada);
-	vector<bitset<64>*> bytesLeidos  = leerArchivo(argv[1]);
-	printf("Tamanio: %lu\n",tamanio);
-	escribirArchivo(bytesLeidos,"encriptado",tamanio);
+	else{
+		cout << "./a.out nombreArchivoOriginal nombreArchivoEncriptado" << endl;
+	}
 	return 0;
-	
-	/******Aqui se implementara algo para encriptar archivos.*******/
-
-	
 }
