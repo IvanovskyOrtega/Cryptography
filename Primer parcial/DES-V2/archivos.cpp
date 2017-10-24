8bac313397cfaf19738b83bdd524dfb9e47b5d1a
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-10-23 22:31:15 -0500

    Funciones de escritura, lectura y apertura de archivos

--- Primer parcial/DES-V2/archivos.cpp
+++ Primer parcial/DES-V2/archivos.cpp
@@ -8,28 +8,59 @@ namespace archivos{
 	size_t tamanio;
 }
 
-vector<uchar*> archivos::leerBytes(string nombre){
-	size_t numeroDeBytesLeidos;
-	uchar* aux = new uchar();
-	vector<uchar *> buffer;
+void archivos::abrirArchivoLectura(ifstream* archivo, string nombre){
 	try{
-		ifstream input(nombre, std::ios::binary|std::ios::ate);
-		if(input.is_open()){
-			input.seekg(0, ios::end);
-			numeroDeBytesLeidos = input.tellg();
-			tamanio = numeroDeBytesLeidos;
-			cout << "Se leyeron " << numeroDeBytesLeidos << " bytes." << endl;
-			input.seekg(0, ios::beg);
-			while(numeroDeBytesLeidos > 0){
-					input.read((char *)aux, 1);
-					buffer.reserve(1);
-					buffer.push_back(new uchar(*aux));
-					numeroDeBytesLeidos --;
-			}
-			input.close();
+		archivo->open(nombre, ios::binary);
+		if(archivo->is_open()){
+			cout << "Archivo original se abrio correctamente" << endl;
+			archivo->seekg(0, ios::end);
+			tamanio = archivo->tellg();
+			cout << "Se leyeron " << tamanio << " bytes." << endl;
+			archivo->seekg(0, ios::beg);
 		}
-		return buffer;
+		else{
+			cout << "Error al abrir " << nombre <<  endl;
+			exit(0);
+		}
+	}catch(const std::invalid_argument& ia){
+		cerr << "Invalid argument: " << ia.what() << '\n';
+		exit(0);
+	}
+}
 
+void archivos::abrirArchivoEscritura(ofstream* archivo, string nombre){
+	try{
+		archivo->open(nombre, std::ios::binary|std::ios::ate);
+		if(archivo->is_open()){
+			cout << "Archivo de salida se abrio correctamente" << endl;
+		}
+		else{
+			cout << "Error al abrir " << nombre <<  endl;
+			exit(0);
+		}
+	}catch(const std::invalid_argument& ia){
+		cerr << "Invalid argument: " << ia.what() << '\n';
+		exit(0);
+	}
+}
+
+uchar* archivos::leerBytes(ifstream* archivo){
+	int i = 0;
+	uchar* aux = new uchar();
+	uchar * buffer = new uchar[8];
+	try{
+		if(archivo->good()){
+			while(i < 8){
+					archivo->read((char *)aux, 1);
+					buffer[i] = (*aux);
+					i++;
+					return buffer;
+			}
+		}
+		else{
+			cout << "Error durante la lectura" << endl;
+			exit(0);
+		}
 	}catch(const std::invalid_argument& ia){
 		cerr << "Invalid argument: " << ia.what() << '\n';
 		return buffer;
@@ -37,109 +68,21 @@ vector<uchar*> archivos::leerBytes(string nombre){
 	return buffer;
 }
 
-void archivos::escribirBytes(vector<uchar*> bytes, string nombre){
+void archivos::escribirBytes(ofstream* archivo, vector<uchar*> bytes, int numeroDeBytes){
+	int i = 0;
 	try{
-		int i = 0;
-		ofstream archivoEncriptado (nombre, ios::binary);
-		if(archivoEncriptado.is_open()){
-			while(tamanio > 0){
-					archivoEncriptado.write((const char*)bytes[i], 1);
-					tamanio --;
-					i++;
+		if(archivo->good()){
+			while(i < numeroDeBytes){
+				archivo->write((const char*)bytes[i], 1);
+				i++;
 			}
-			archivoEncriptado.close();
 		}
 		else{
-			cout << "Error al crear el archivo." << endl;
-			return;
+			cout << "Error durante la escritura" << endl;
+			exit(0);
 		}
-	}
-	catch(const std::invalid_argument& ia){
+	}catch(const std::invalid_argument& ia){
 		cerr << "Invalid argument: " << ia.what() << '\n';
-		return;
+		exit(0);
 	}
 }
-
-vector<bitset<64>*> archivos::leerArchivo(string nombre){
-	size_t numeroDeBytesLeidos;
-	vector<bitset<64>*> binario;
-	bitset<64>* bytesLeidos = new (nothrow)bitset<64>();
-	try{
-		ifstream input(nombre,std::ios::binary|std::ios::ate);
-		if(input.is_open()){
-			input.seekg(0, ios_base::end);
-			numeroDeBytesLeidos = input.tellg();
-			tamanio = numeroDeBytesLeidos;
-			cout << "Se leyeron " << numeroDeBytesLeidos << " bytes." << endl;
-			input.seekg(0, ios::beg);
-			while(numeroDeBytesLeidos > 0){
-				if(numeroDeBytesLeidos >= 8){
-					input.read((char*)bytesLeidos, 8);
-					binario.reserve(1);
-					binario.push_back(new bitset<64>(*bytesLeidos));
-					bytesLeidos->reset();
-					numeroDeBytesLeidos -= 8;
-				}
-				else{
-					input.read((char *)bytesLeidos, numeroDeBytesLeidos);
-					binario.reserve(1);
-					binario.push_back(new bitset<64>(*bytesLeidos));
-					numeroDeBytesLeidos -= 8;
-					bytesLeidos->reset();
-					break;
-				}
-			}
-			input.close();
-		}
-		else{
-			cout << "No se pudo abrir el archivo." << endl;
-			return binario;
-		}
-		return binario;
-	
-	}
-	catch(const std::invalid_argument& ia){
-		cerr << "Invalid argument: " << ia.what() << '\n';
-		return binario;
-	}	
-}
-
-void archivos::escribirArchivo(vector<bitset<64>*> bytes, string nombre, int tamanioBytes){
-	try{
-		int i = 0;
-		ofstream archivoEncriptado (nombre, ios::binary);
-		if(archivoEncriptado.is_open()){
-			while(tamanioBytes > 0){
-				if(tamanioBytes >= 8){
-					archivoEncriptado.write((const char*)bytes[i], 8);
-					tamanioBytes -= 8;
-					i++;
-				}
-				else{
-					archivoEncriptado.write((const char*)bytes[i], tamanioBytes);
-					tamanioBytes -= 8;
-					i++;
-					break;
-				}
-			}
-			archivoEncriptado.close();
-		}
-		else{
-			cout << "Error al crear el archivo." << endl;
-			return;
-		}
-	}
-	catch(const std::invalid_argument& ia){
-		cerr << "Invalid argument: " << ia.what() << '\n';
-		return;
-	}
-}
-
-void archivos::establecerTamanio(int valor){
-	archivos::tamanio = valor;
-}
-
-int archivos::obtenerTamanio(){
-	return archivos::tamanio;
-}
-
