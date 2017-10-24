66ce08f81c8ca95049b06f779a92c3e6bf95be1b
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-10-23 22:33:50 -0500

    bits.hpp

--- Primer parcial/DES-V2/bits.hpp
+++ Primer parcial/DES-V2/bits.hpp
@@ -1,11 +1,14 @@
 #ifndef _BITS_HPP_
 #define _BITS_HPP_
 
+#include<iostream>
+
 namespace bits{
     typedef unsigned char uchar;
     void inicializarBytes(uchar *bytes, int numBytes);
     void poner0(uchar* byte, int pos);
     void poner1(uchar* byte, int pos);
+    void ponerBit(uchar* byte, int bit, int pos);
     int consultarBit(uchar byte, int pos);
     void imprimirByte(uchar byte);
 }
