a6ff503c683a35955bb12f313d7b7e5488580bb7
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-10-27 10:28:31 -0500

    Funciones de manejo de bits

--- Primer parcial/DES-V3/bits.cpp
+++ Primer parcial/DES-V3/bits.cpp
@@ -4,14 +4,16 @@ using namespace std;
 using namespace bits;
 
 void bits::poner1(u64 *byte, int pos){
+    u64 a = 1;
     if(!consultarBit((*byte),pos)){
-        (*byte) ^= (1<<pos);
+        (*byte) ^= (a<<pos);
     }
 }
 
 void bits::poner0(u64 *byte, int pos){
+    u64 a = 1;
     if(consultarBit((*byte),pos)){
-        (*byte) ^= (1<<pos);
+        (*byte) ^= (a<<pos);
     }
 }
 
@@ -25,7 +27,7 @@ void bits::ponerBit(u64* byte, int bit, int pos){
 }
 
 int bits::consultarBit(u64 byte, int pos){
-    u64 a = 1; 
+    u64 a = 0x0000000000000001; 
     if((byte)&(a<<pos)){
         return 1;
     }
@@ -36,5 +38,18 @@ void bits::imprimiru64(u64 byte){
     cout << "|";
     for(int i = 63 ; i >= 0 ; i--){
         cout << consultarBit(byte,i);
+        if(i%8==0){
+            cout << "|";
+        }
+    }
+}
+
+void bits::imprimiru32(u32 byte){
+    cout << "|";
+    for(int i = 31 ; i >= 0 ; i--){
+        cout << consultarBit(byte,i);
+        if(i%8==0){
+            cout << "|";
+        }
     }
 }
