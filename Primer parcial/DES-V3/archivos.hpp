9d0eef7fcd7936f1dead2b0a63bfc214f4580e08
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-10-27 10:27:25 -0500

    archivos.hpp

--- Primer parcial/DES-V3/archivos.hpp
+++ Primer parcial/DES-V3/archivos.hpp
@@ -9,12 +9,15 @@
 #include "bits.hpp"
 
 namespace archivos{
+    typedef uint64_t u64;
     typedef unsigned char uchar;
     extern size_t tamanio;
     void abrirArchivoLectura(std::ifstream* archivo, std::string nombre);
     void abrirArchivoEscritura(std::ofstream* archivo, std::string nombre);
     uchar* leerBytes(std::ifstream* archivo);
     void escribirBytes(std::ofstream* archivo, std::vector<uchar*> bytes, int numeroDeBytes);
+    void escribirBytesU64(std::ofstream* archivo, u64* bytes);
+    u64 uchar_to_u64(uchar* bytes);
 }
 
 #endif
