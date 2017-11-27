60734d6ffc64c85315798a0d472fb1f8d56ae1b6
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-11-27 09:20:14 -0600

    algoritmos.hpp

--- Segundo-Parcial/Miller-Rabin/algoritmos.hpp
+++ Segundo-Parcial/Miller-Rabin/algoritmos.hpp
@@ -1,5 +1,7 @@
 #ifndef _ALGORITMOS_HPP_
 #define _ALGORITMOS_HPP_
+#ifndef _ALGORITMOS_HPP_
+#define _ALGORITMOS_HPP_
 
 #include <vector>
 #include <iostream>
@@ -12,6 +14,7 @@ typedef long long ll;
 
 namespace algoritmos{
 	extern std::vector<ll> Qn;
+	extern std::vector<ll*> sumas;
 	ll algoritmoDeEuclides(ll r0, ll r1 );
 	ll encontrarInverso(ll r0);
 	ll potenciar(ll b, ll c, ll n);
@@ -20,6 +23,10 @@ namespace algoritmos{
 	bool millerRabin(ll n, ll m, ll k);
 	bool esPrimo(ll n, ll iteraciones);
 	void menu(char opcion);
+	void curvaEliptica(ll a, ll b);
+	void verificarK(ll k, ll p, ll x0, ll y0, ll q);
+	ll* doblar(ll x0, ll y0, ll x1, ll y1, ll k, ll p);
+	ll* sumar(ll numSumas, ll x0, ll y0, ll x1, ll y1, ll k, ll p);
 }
 
 #endif
